a435765863da8e11a9d52b28fb4940f66d30eab7
Strider221B <somesh.chatterjee@gmail.com>
2025-07-01 13:44:52 +0000

    bug fixes

--- foundations_01/chat_agent_with_validator_03.py
+++ foundations_01/chat_agent_with_validator_03.py
@@ -10,10 +10,12 @@ from common.response_formats.evaluation import Evaluation
 from foundations_01.helpers import Helpers
 from foundations_01.simple_chat_agent_02 import SimpleChatAgent
 
+load_dotenv(override=True)
+
 class ChatAgentWithValidator:
 
     def __init__(self, name: str, linked_in_path: str, summary_path: str):
-        self._chat_agent = SimpleChatAgent()
+        self._chat_agent = SimpleChatAgent(name, linked_in_path, summary_path)
         self._eval_model = OpenAI(base_url=Constants.GEMINI_BASE_URL, api_key=os.getenv(Constants.GOOGLE_API_KEY))
         self._name = name
         self._linked_in_path = linked_in_path
@@ -28,6 +30,7 @@ class ChatAgentWithValidator:
 
         if evaluation.is_acceptable:
             print("Passed evaluation - returning reply")
+            print(evaluation.feedback)
         else:
             print("Failed evaluation - retrying")
             print(evaluation.feedback)
@@ -35,7 +38,7 @@ class ChatAgentWithValidator:
         return reply
 
     @staticmethod
-    def _get_evaluation_system_prompt(name: str, summary: str, linkedin: str):
+    def _get_evaluation_system_prompt(name: str, summary: str, linkedin: str) -> str:
         evaluator_system_prompt = (f"You are an evaluator that decides whether a response to a question is acceptable. "
                                    f"You are provided with a conversation between a User and an Agent. "
                                    f"Your task is to decide whether the Agent's latest response is acceptable quality. "
@@ -47,9 +50,10 @@ class ChatAgentWithValidator:
 
         evaluator_system_prompt += f"\n\n## Summary:\n{summary}\n\n## LinkedIn Profile:\n{linkedin}\n\n"
         evaluator_system_prompt += f"With this context, please evaluate the latest response, replying with whether the response is acceptable and your feedback."
+        return evaluator_system_prompt
 
     @staticmethod
-    def _get_evaluator_user_prompt(reply: str, message: str, history: List[str]):
+    def _get_evaluator_user_prompt(reply: str, message: str, history: List[str]) -> str:
         user_prompt = f"Here's the conversation between the User and the Agent: \n\n{history}\n\n"
         user_prompt += f"Here's the latest message from the User: \n\n{message}\n\n"
         user_prompt += f"Here's the latest response from the Agent: \n\n{reply}\n\n"
@@ -62,8 +66,8 @@ class ChatAgentWithValidator:
                       Constants.OPEN_AI_CONTENT: self._eval_system_prompt}]
                     + [{Constants.OPEN_AI_ROLE: Constants.OPEN_AI_USER,
                         Constants.OPEN_AI_CONTENT: self._get_evaluator_user_prompt(reply, message, history)}])
-        response = self._open_ai.chat.completions.create(model=Constants.GEMINI_MODEL_LITE, messages=messages, response_format=Evaluation)
-        return response.choices[0].message.content
+        response = self._eval_model.beta.chat.completions.parse(model=Constants.GEMINI_MODEL_LITE, messages=messages, response_format=Evaluation)
+        return response.choices[0].message.parsed
 
 if __name__ == '__main__':
-    gr.ChatInterface(SimpleChatAgent('Ed Donner', './me/linkedin.pdf', './me/summary.txt').chat, type='messages').launch()
+    gr.ChatInterface(ChatAgentWithValidator('Ed Donner', './me/linkedin.pdf', './me/summary.txt').chat, type='messages').launch()
