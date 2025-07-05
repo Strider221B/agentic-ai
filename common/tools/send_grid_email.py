a550fd16bbb733b0382cecc8d2c2b873c9053572
Strider221B <somesh.chatterjee@gmail.com>
2025-07-05 08:07:41 +0000

    send email using handovers

--- common/tools/send_grid_email.py
+++ common/tools/send_grid_email.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 import sendgrid
-from agents import function_tool
+from agents import Agent, OpenAIChatCompletionsModel, function_tool
 from sendgrid.helpers.mail import Mail, Email, To, Content
 
 from common.constants import Constants
@@ -11,6 +11,8 @@ load_dotenv(override=True)
 
 class SendGridEmail:
 
+    EMAIL_TYPE_HTML = 'text/html'
+    EMAIL_TYPE_PLAIN = 'text/plain'
     _EMAIL_ID = os.environ.get(Constants.EMAIL_ID)
     _SENDGRID_KEY = os.environ.get(Constants.SENDGRID_API_KEY)
 
@@ -18,18 +20,39 @@ class SendGridEmail:
     @function_tool
     def send_email_tool(subject: str, mail_body: str):
         # function_tool cannot be called directly so have split it into 2 separate methods.
-        SendGridEmail.send_email(subject, mail_body, None)
+        SendGridEmail.send_email(subject, mail_body, SendGridEmail.EMAIL_TYPE_PLAIN, None)
+
+    @staticmethod
+    @function_tool
+    def send_email_tool_html(subject: str, mail_body: str):
+        # function_tool cannot be called directly so have split it into 2 separate methods.
+        SendGridEmail.send_email(subject, mail_body, SendGridEmail.EMAIL_TYPE_HTML, None)
 
     @classmethod
-    def send_email(cls, subject: str, mail_body: str, to_email_id: str = None):
+    def get_html_converter_tool(cls, model: OpenAIChatCompletionsModel):
+        html_converter = Agent(name="HTML email body converter",
+                               instructions=cls._get_html_email_instructions(),
+                               model=model)
+        html_tool = html_converter.as_tool(tool_name="html_converter",
+                                           tool_description="Convert a text email body to an HTML email body")
+        return html_tool
+
+    @classmethod
+    def send_email(cls, subject: str, mail_body: str, email_type: str, to_email_id: str = None):
         to_email_id = to_email_id if to_email_id else cls._EMAIL_ID
         sg = sendgrid.SendGridAPIClient(api_key=cls._SENDGRID_KEY)
         from_email = Email(cls._EMAIL_ID)
         to_email = To(to_email_id)
-        content = Content("text/plain", mail_body)
+        content = Content(email_type, mail_body)
         mail = Mail(from_email, to_email, subject, content).get()
         response = sg.client.mail.send.post(request_body=mail)
         print(f'Email sent to: {to_email_id} - Response Code: {response.status_code}')
+
+    @staticmethod
+    def _get_html_email_instructions() -> str:
+        return ("You can convert a text email body to an HTML email body. "
+                "You are given a text email body which might have some markdown "
+                "and you need to convert it to an HTML email body with simple, clear, compelling layout and design.")
 
 if __name__ == '__main__':
     SendGridEmail.send_email('This is a test email', 'Test Mail')
--- open_ai_02/sales_agent_02.py
+++ open_ai_02/sales_agent_02.py
@@ -1,13 +1,13 @@
 from dotenv import load_dotenv
-from agents import Agent, Runner, Tool, trace, function_tool
-from openai.types.responses import ResponseTextDeltaEvent
-from typing import Dict, List
-import os
+from agents import Agent, Runner, RunResult, Tool
+from typing import List
 import asyncio
 
 from common.constants import Constants
 from common.open_ai_gemini_client import OpenAIGeminiClient
 from common.tools.send_grid_email import SendGridEmail
+
+load_dotenv(override=True)
 
 class SalesAgent:
 
@@ -38,8 +38,24 @@ class SalesAgent:
         result = asyncio.run(self._send_email_as_tools_async())
         print(result)
 
-    async def _send_email_as_tools_async(self):
-        tools = self._get_agents_and_email_tools()
+    def send_email_using_handovers(self):
+        result = asyncio.run(self._send_emails_using_handoffs())
+        print(result)
+
+    async def _send_emails_using_handoffs(self) -> RunResult:
+        emailer_agent = self._get_emailer_agent()
+        handoffs = [emailer_agent]
+        sales_agent_tools = self._get_sales_agents_as_tools()
+        sales_manager = Agent(name="Sales Manager",
+                              instructions=self._get_sales_manager_instructions_with_handovers(),
+                              tools=sales_agent_tools,
+                              handoffs=handoffs,
+                              model=OpenAIGeminiClient.get_model())
+        message = "Send out a cold sales email addressed to Dear CEO from Alice"
+        return await Runner.run(sales_manager, message)
+
+    async def _send_email_as_tools_async(self) -> RunResult:
+        tools = self._get_agents_and_send_email_as_tools()
         sales_manager = Agent(name="Sales Manager",
                               instructions=self._get_send_email_instructions_using_tool(),
                               tools=tools,
@@ -47,13 +63,6 @@ class SalesAgent:
         message = "Send a cold sales email addressed to 'Dear CEO'"
         result = await Runner.run(sales_manager, message)
         return result
-
-    def _get_agents_and_email_tools(self) -> List[Tool]:
-        description = "Write a cold sales email"
-        tool1 = self._prof_agent.as_tool(tool_name="sales_agent1", tool_description=description)
-        tool2 = self._witty_agent.as_tool(tool_name="sales_agent2", tool_description=description)
-        tool3 = self._concise_agent.as_tool(tool_name="sales_agent3", tool_description=description)
-        return [tool1, tool2, tool3, SendGridEmail.send_email_tool]
 
     async def _select_best_email_for(self, message: str):
         all_emails = await self._generate_email_using_all_agents(message)
@@ -68,6 +77,36 @@ class SalesAgent:
         results = [result.final_output for result in results]
         return results
 
+    def _get_agents_and_send_email_as_tools(self) -> List[Tool]:
+        tools = self._get_sales_agents_as_tools()
+        tools.append(SendGridEmail.send_email_tool)
+        return tools
+
+    def _get_sales_agents_as_tools(self) -> List[Tool]:
+        description = "Write a cold sales email"
+        tool1 = self._prof_agent.as_tool(tool_name="sales_agent1", tool_description=description)
+        tool2 = self._witty_agent.as_tool(tool_name="sales_agent2", tool_description=description)
+        tool3 = self._concise_agent.as_tool(tool_name="sales_agent3", tool_description=description)
+        return [tool1, tool2, tool3]
+
+    @classmethod
+    def _get_emailer_agent(cls) -> Agent:
+        subject_writer = Agent(name="Email subject writer",
+                               instructions=cls._get_subject_writer_instructions(),
+                               model=OpenAIGeminiClient.get_model())
+        subject_tool = subject_writer.as_tool(tool_name="subject_writer",
+                                              tool_description="Write a subject for a cold sales email")
+        tools = [subject_tool,
+                 SendGridEmail.get_html_converter_tool(OpenAIGeminiClient.get_model()),
+                 SendGridEmail.send_email_tool_html]
+        emailer_agent = Agent(name="Email Manager",
+                              instructions=cls._get_emailer_agent_instructions(),
+                              tools=tools,
+                              model=OpenAIGeminiClient.get_model(),
+                              handoff_description="Convert an email to HTML and send it")
+
+        return emailer_agent
+
     @staticmethod
     def _get_send_email_instructions_using_tool():
         return ("You are a sales manager working for ComplAI. You use the tools given to you to generate cold sales emails. "
@@ -76,6 +115,17 @@ class SalesAgent:
                 "Do not worry on the exact content of the emails, the sales_agent tools already have the instructions to draft the email. "
                 "Just ask the sales_agent tools to come up with the email. "
                 "You pick the single best email and use the send_email tool to send the best email (and only the best email) to the user.")
+
+    @staticmethod
+    def _get_sales_manager_instructions_with_handovers() -> str:
+        return ("You are a sales manager working for ComplAI. You use the tools given to you to generate cold sales emails. "
+                "You never generate sales emails yourself; you always use the tools. "
+                "You try all 3 sales agent tools at least once before choosing the best one. "
+                "Do not worry on the exact content of the emails, the sales_agent tools already have the instructions to draft the email. "
+                "Just ask the sales_agent tools to come up with the email. "
+                "You can use the tools multiple times if you're not satisfied with the results from the first try. "
+                "You select the single best email using your own judgement of which email will be most effective. "
+                "After picking the email, you handoff to the Email Manager agent to format and send the email.")
 
     @staticmethod
     def _get_professional_instruction() -> str:
@@ -101,11 +151,24 @@ class SalesAgent:
                 "customer and pick the one you are most likely to respond to. Do not give "
                 "an explanation; reply with the selected email only.")
 
+    @staticmethod
+    def _get_subject_writer_instructions() -> str:
+        return ("You can write a subject for a cold sales email. "
+                "You are given a message and you need to write a subject for an email that is likely to get a response.")
+
+    @staticmethod
+    def _get_emailer_agent_instructions() -> str:
+        return ("You are an email formatter and sender. You receive the body of an email to be sent. "
+                "You first use the subject_writer tool to write a subject for the email, "
+                "then use the html_converter tool to convert the body to HTML. "
+                "Finally, you use the send_html_email tool to send the email with the subject and HTML body.")
+
 if __name__ == '__main__':
     sales_agent = SalesAgent()
     # sales_agent.print_sample_email_for_all_agents("Write a cold sales email")
     # sales_agent.print_best_email_from_all_agent("Write a cold sales email")
-    sales_agent.send_email_as_tools()
+    # sales_agent.send_email_as_tools()
+    sales_agent.send_email_using_handovers()
 
 # Output:
 # sales_agent.send_email_as_tools() -->
@@ -117,6 +180,23 @@ if __name__ == '__main__':
 #     I have sent the email to the CEO.
 # - 9 new item(s)
 # - 3 raw response(s)
+# - 0 input guardrail result(s)
+# - 0 output guardrail result(s)
+# (See `RunResult` for more details)
+# OPENAI_API_KEY is not set, skipping trace export
+
+# sales_agent.send_email_using_handovers() -->
+# OPENAI_API_KEY is not set, skipping trace export
+# OPENAI_API_KEY is not set, skipping trace export
+# OPENAI_API_KEY is not set, skipping trace export
+# OPENAI_API_KEY is not set, skipping trace export
+# Email sent to: somesh_chatterjee2003@yahoo.co.in - Response Code: 202
+# RunResult:
+# - Last agent: Agent(name="Email Manager", ...)
+# - Final output (str):
+#     I have sent the email to the CEO.
+# - 16 new item(s)
+# - 8 raw response(s)
 # - 0 input guardrail result(s)
 # - 0 output guardrail result(s)
 # (See `RunResult` for more details)
