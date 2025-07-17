ae4dc0e86b6a6c4fe6cc7566de8b06570dd15794
Strider221B <somesh.chatterjee@gmail.com>
2025-07-17 05:26:35 +0000

    Added memory support as well to stock picker.

--- crew_ai_03/stock_picker_03/src/stock_picker_03/crew.py
+++ crew_ai_03/stock_picker_03/src/stock_picker_03/crew.py
@@ -1,10 +1,15 @@
+import os
 from typing import List
 
 from crewai import Agent, Crew, Process, Task
 from crewai.agents.agent_builder.base_agent import BaseAgent
+from crewai.memory import LongTermMemory, ShortTermMemory, EntityMemory
+from crewai.memory.storage.rag_storage import RAGStorage
+from crewai.memory.storage.ltm_sqlite_storage import LTMSQLiteStorage
 from crewai.project import CrewBase, agent, crew, task
 from crewai_tools import SerperDevTool
 
+from common.constants import Constants
 from common.response_formats.trending_company_list import TrendingCompanyList
 from common.response_formats.trending_company_research_list import TrendingCompanyResearchList
 from tools.push_notification_tool import PushNotificationTool
@@ -18,7 +23,7 @@ class StockPicker03():
 
     @agent
     def trending_company_finder(self) -> Agent:
-        return Agent(config=self.agents_config['trending_company_finder'], tools=[SerperDevTool()])
+        return Agent(config=self.agents_config['trending_company_finder'], tools=[SerperDevTool()], memory=True)
 
     @agent
     def financial_researcher(self) -> Agent:
@@ -27,7 +32,7 @@ class StockPicker03():
 
     @agent
     def stock_picker(self) -> Agent:
-        return Agent(config=self.agents_config['stock_picker'], tools=[PushNotificationTool()])
+        return Agent(config=self.agents_config['stock_picker'], tools=[PushNotificationTool()], memory=True)
 
     @task
     def find_trending_companies(self) -> Task:
@@ -50,8 +55,17 @@ class StockPicker03():
             allow_delegation=True
         )
 
+        embedder_config = {"provider": "google",
+                           "config": {"model": "models/gemini-embedding-001",
+                                      "api_key": os.getenv(Constants.GOOGLE_API_KEY) }}
+
         return Crew(agents=self.agents,
                     tasks=self.tasks,
                     process=Process.hierarchical,
                     verbose=True,
-                    manager_agent=manager)
+                    manager_agent=manager,
+                    memory=True,
+                    long_term_memory=LongTermMemory(storage=LTMSQLiteStorage('./memory/long_term_memory_storage.db')),
+                    short_term_memory=ShortTermMemory(storage=RAGStorage(type='short_term', path='./memory', embedder_config=embedder_config)),
+                    entity_memory=EntityMemory(storage=RAGStorage(type='short_term', path='./memory', embedder_config=embedder_config)),
+                    embedder=embedder_config)
